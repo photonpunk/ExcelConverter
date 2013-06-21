39756fec4b6441961ab582356bcd028ef4c619ae
Austin <alienshade@gmail.com>
2013-06-21 17:02:08 -0500

    Update ExcelConverter.pl

--- ExcelConverter.pl
+++ ExcelConverter.pl
@@ -1,5 +1,24 @@
-# created by Austin Dixon 5/13/2013
-#
+##  Copyright (c) 2013, The University of Alabama Libraries.
+##  Contributed by {Austin Dixon}  {5/13/2013}.
+##  All rights reserved.
+ 
+##  Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ 
+##    * Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+##    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the
+##       distribution.
+##    * Neither the name of The University of Alabama Libraries nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ 
+##THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+##THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+##CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+##PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+##LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+##EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+
+
 # script creates either an tab-delimited, unicode tab-delimited, csv, or xml file from a user selected excel spreadsheet
 # Unicode option also parses and removes only the unwanted quotes added by the Excel export. the *good* quotes within a cell are retained
 # Unicode option now re-encodes the exported UCS-2 16bit little endian into standard UTF-8 without BOM
